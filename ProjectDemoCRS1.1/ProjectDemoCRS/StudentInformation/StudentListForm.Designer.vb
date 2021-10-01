e5cc074d02d98c35e545641ebfc9b45bae42b2cf
sorehahhh <78630154+sorehahhh@users.noreply.github.com>
2021-10-01 18:32:19 +0800

    update

--- ProjectDemoCRS1.1/ProjectDemoCRS/StudentInformation/StudentListForm.Designer.vb
+++ ProjectDemoCRS1.1/ProjectDemoCRS/StudentInformation/StudentListForm.Designer.vb
@@ -22,7 +22,6 @@ Partial Class StudentListForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(StudentListForm))
         Me.updateStudentButton = New System.Windows.Forms.Button()
         Me.AddStudentButton = New System.Windows.Forms.Button()
@@ -34,36 +33,8 @@ Partial Class StudentListForm
         Me.ICButton = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.searchTextBox = New System.Windows.Forms.TextBox()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.CloseButton = New System.Windows.Forms.Button()
-        Me.DeleteButton = New System.Windows.Forms.Button()
-        Me.SaveButton = New System.Windows.Forms.Button()
-        Me.NextButton = New System.Windows.Forms.Button()
-        Me.AddNewButton = New System.Windows.Forms.Button()
-        Me.PreviousButton = New System.Windows.Forms.Button()
-        Me.dobDateTimePicker1 = New System.Windows.Forms.DateTimePicker()
-        Me.nameTextBox = New System.Windows.Forms.TextBox()
-        Me.PhoneNumberTextBox = New System.Windows.Forms.TextBox()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.icNumberTextBox = New System.Windows.Forms.TextBox()
-        Me.matricTextBox = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.IhsanTuitionCenterDbDataSet = New ProjectDemoCRS.ihsanTuitionCenterDbDataSet()
-        Me.IhsanTuitionCenterDbDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.StudentBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.StudentTableAdapter = New ProjectDemoCRS.ihsanTuitionCenterDbDataSetTableAdapters.studentTableAdapter()
-        Me.StudentBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
-        Me.MatricNumberDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.StudentDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Search.SuspendLayout()
-        Me.GroupBox1.SuspendLayout()
-        CType(Me.IhsanTuitionCenterDbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.IhsanTuitionCenterDbDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.StudentBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.StudentBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'updateStudentButton
@@ -104,18 +75,14 @@ Partial Class StudentListForm
         '
         'StudentDataGridView
         '
-        Me.StudentDataGridView.AllowUserToOrderColumns = True
-        Me.StudentDataGridView.AutoGenerateColumns = False
         Me.StudentDataGridView.BackgroundColor = System.Drawing.Color.Turquoise
         Me.StudentDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.StudentDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.MatricNumberDataGridViewTextBoxColumn})
-        Me.StudentDataGridView.DataSource = Me.StudentBindingSource
-        Me.StudentDataGridView.Location = New System.Drawing.Point(672, 166)
+        Me.StudentDataGridView.Location = New System.Drawing.Point(52, 146)
         Me.StudentDataGridView.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.StudentDataGridView.MultiSelect = False
         Me.StudentDataGridView.Name = "StudentDataGridView"
         Me.StudentDataGridView.RowHeadersWidth = 49
-        Me.StudentDataGridView.Size = New System.Drawing.Size(671, 416)
+        Me.StudentDataGridView.Size = New System.Drawing.Size(1656, 416)
         Me.StudentDataGridView.TabIndex = 9
         '
         'Search
@@ -181,227 +148,15 @@ Partial Class StudentListForm
         Me.searchTextBox.Location = New System.Drawing.Point(293, 49)
         Me.searchTextBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.searchTextBox.Name = "searchTextBox"
-        Me.searchTextBox.Size = New System.Drawing.Size(392, 38)
+        Me.searchTextBox.Size = New System.Drawing.Size(392, 37)
         Me.searchTextBox.TabIndex = 0
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.CloseButton)
-        Me.GroupBox1.Controls.Add(Me.DeleteButton)
-        Me.GroupBox1.Controls.Add(Me.SaveButton)
-        Me.GroupBox1.Controls.Add(Me.NextButton)
-        Me.GroupBox1.Controls.Add(Me.AddNewButton)
-        Me.GroupBox1.Controls.Add(Me.PreviousButton)
-        Me.GroupBox1.Controls.Add(Me.dobDateTimePicker1)
-        Me.GroupBox1.Controls.Add(Me.nameTextBox)
-        Me.GroupBox1.Controls.Add(Me.PhoneNumberTextBox)
-        Me.GroupBox1.Controls.Add(Me.Label6)
-        Me.GroupBox1.Controls.Add(Me.Label5)
-        Me.GroupBox1.Controls.Add(Me.icNumberTextBox)
-        Me.GroupBox1.Controls.Add(Me.matricTextBox)
-        Me.GroupBox1.Controls.Add(Me.Label3)
-        Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Controls.Add(Me.Label4)
-        Me.GroupBox1.Location = New System.Drawing.Point(16, 166)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.GroupBox1.Size = New System.Drawing.Size(648, 395)
-        Me.GroupBox1.TabIndex = 14
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
-        '
-        'CloseButton
-        '
-        Me.CloseButton.Location = New System.Drawing.Point(453, 304)
-        Me.CloseButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.CloseButton.Name = "CloseButton"
-        Me.CloseButton.Size = New System.Drawing.Size(99, 37)
-        Me.CloseButton.TabIndex = 37
-        Me.CloseButton.Text = "Close"
-        Me.CloseButton.UseVisualStyleBackColor = True
-        '
-        'DeleteButton
-        '
-        Me.DeleteButton.Location = New System.Drawing.Point(243, 304)
-        Me.DeleteButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.DeleteButton.Name = "DeleteButton"
-        Me.DeleteButton.Size = New System.Drawing.Size(99, 37)
-        Me.DeleteButton.TabIndex = 36
-        Me.DeleteButton.Text = "Delete"
-        Me.DeleteButton.UseVisualStyleBackColor = True
-        '
-        'SaveButton
-        '
-        Me.SaveButton.Location = New System.Drawing.Point(60, 304)
-        Me.SaveButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.SaveButton.Name = "SaveButton"
-        Me.SaveButton.Size = New System.Drawing.Size(99, 37)
-        Me.SaveButton.TabIndex = 35
-        Me.SaveButton.Text = "Save"
-        Me.SaveButton.UseVisualStyleBackColor = True
-        '
-        'NextButton
-        '
-        Me.NextButton.Location = New System.Drawing.Point(453, 240)
-        Me.NextButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.NextButton.Name = "NextButton"
-        Me.NextButton.Size = New System.Drawing.Size(99, 37)
-        Me.NextButton.TabIndex = 34
-        Me.NextButton.Text = "Next"
-        Me.NextButton.UseVisualStyleBackColor = True
-        '
-        'AddNewButton
-        '
-        Me.AddNewButton.Location = New System.Drawing.Point(243, 240)
-        Me.AddNewButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.AddNewButton.Name = "AddNewButton"
-        Me.AddNewButton.Size = New System.Drawing.Size(99, 37)
-        Me.AddNewButton.TabIndex = 33
-        Me.AddNewButton.Text = "Add New"
-        Me.AddNewButton.UseVisualStyleBackColor = True
-        '
-        'PreviousButton
-        '
-        Me.PreviousButton.Location = New System.Drawing.Point(60, 240)
-        Me.PreviousButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.PreviousButton.Name = "PreviousButton"
-        Me.PreviousButton.Size = New System.Drawing.Size(99, 37)
-        Me.PreviousButton.TabIndex = 32
-        Me.PreviousButton.Text = "Previous"
-        Me.PreviousButton.UseVisualStyleBackColor = True
-        '
-        'dobDateTimePicker1
-        '
-        Me.dobDateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.dobDateTimePicker1.Location = New System.Drawing.Point(157, 133)
-        Me.dobDateTimePicker1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.dobDateTimePicker1.Name = "dobDateTimePicker1"
-        Me.dobDateTimePicker1.Size = New System.Drawing.Size(345, 22)
-        Me.dobDateTimePicker1.TabIndex = 31
-        '
-        'nameTextBox
-        '
-        Me.nameTextBox.Location = New System.Drawing.Point(159, 97)
-        Me.nameTextBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.nameTextBox.Name = "nameTextBox"
-        Me.nameTextBox.Size = New System.Drawing.Size(348, 22)
-        Me.nameTextBox.TabIndex = 30
-        '
-        'PhoneNumberTextBox
-        '
-        Me.PhoneNumberTextBox.Location = New System.Drawing.Point(159, 172)
-        Me.PhoneNumberTextBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.PhoneNumberTextBox.Name = "PhoneNumberTextBox"
-        Me.PhoneNumberTextBox.Size = New System.Drawing.Size(348, 22)
-        Me.PhoneNumberTextBox.TabIndex = 29
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(32, 172)
-        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(103, 17)
-        Me.Label6.TabIndex = 28
-        Me.Label6.Text = "Phone Number"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(32, 133)
-        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(86, 17)
-        Me.Label5.TabIndex = 27
-        Me.Label5.Text = "Date of birth"
-        '
-        'icNumberTextBox
-        '
-        Me.icNumberTextBox.Location = New System.Drawing.Point(159, 62)
-        Me.icNumberTextBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.icNumberTextBox.Name = "icNumberTextBox"
-        Me.icNumberTextBox.Size = New System.Drawing.Size(348, 22)
-        Me.icNumberTextBox.TabIndex = 26
-        '
-        'matricTextBox
-        '
-        Me.matricTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.StudentBindingSource, "matricNumber", True))
-        Me.matricTextBox.Location = New System.Drawing.Point(159, 23)
-        Me.matricTextBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.matricTextBox.Name = "matricTextBox"
-        Me.matricTextBox.Size = New System.Drawing.Size(344, 22)
-        Me.matricTextBox.TabIndex = 25
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(32, 97)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(45, 17)
-        Me.Label3.TabIndex = 24
-        Me.Label3.Text = "Name"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(32, 62)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(72, 17)
-        Me.Label2.TabIndex = 23
-        Me.Label2.Text = "Ic Number"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(32, 27)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(100, 17)
-        Me.Label4.TabIndex = 22
-        Me.Label4.Text = "Matric Number"
-        '
-        'IhsanTuitionCenterDbDataSet
-        '
-        Me.IhsanTuitionCenterDbDataSet.DataSetName = "ihsanTuitionCenterDbDataSet"
-        Me.IhsanTuitionCenterDbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'IhsanTuitionCenterDbDataSetBindingSource
-        '
-        Me.IhsanTuitionCenterDbDataSetBindingSource.DataSource = Me.IhsanTuitionCenterDbDataSet
-        Me.IhsanTuitionCenterDbDataSetBindingSource.Position = 0
-        '
-        'StudentBindingSource
-        '
-        Me.StudentBindingSource.DataMember = "student"
-        Me.StudentBindingSource.DataSource = Me.IhsanTuitionCenterDbDataSetBindingSource
-        '
-        'StudentTableAdapter
-        '
-        Me.StudentTableAdapter.ClearBeforeFill = True
-        '
-        'StudentBindingSource1
-        '
-        Me.StudentBindingSource1.DataMember = "student"
-        Me.StudentBindingSource1.DataSource = Me.IhsanTuitionCenterDbDataSetBindingSource
-        '
-        'MatricNumberDataGridViewTextBoxColumn
-        '
-        Me.MatricNumberDataGridViewTextBoxColumn.DataPropertyName = "matricNumber"
-        Me.MatricNumberDataGridViewTextBoxColumn.HeaderText = "matricNumber"
-        Me.MatricNumberDataGridViewTextBoxColumn.MinimumWidth = 6
-        Me.MatricNumberDataGridViewTextBoxColumn.Name = "MatricNumberDataGridViewTextBoxColumn"
-        Me.MatricNumberDataGridViewTextBoxColumn.Width = 125
         '
         'StudentListForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.ClientSize = New System.Drawing.Size(1371, 750)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.ClientSize = New System.Drawing.Size(1827, 832)
         Me.Controls.Add(Me.Search)
         Me.Controls.Add(Me.updateStudentButton)
         Me.Controls.Add(Me.AddStudentButton)
@@ -413,12 +168,6 @@ Partial Class StudentListForm
         CType(Me.StudentDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Search.ResumeLayout(False)
         Me.Search.PerformLayout()
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
-        CType(Me.IhsanTuitionCenterDbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.IhsanTuitionCenterDbDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.StudentBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.StudentBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -433,27 +182,4 @@ Partial Class StudentListForm
     Friend WithEvents Label1 As Label
     Friend WithEvents searchTextBox As TextBox
     Friend WithEvents displayAllButton As Button
-    Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents CloseButton As Button
-    Friend WithEvents DeleteButton As Button
-    Friend WithEvents SaveButton As Button
-    Friend WithEvents NextButton As Button
-    Friend WithEvents AddNewButton As Button
-    Friend WithEvents PreviousButton As Button
-    Friend WithEvents dobDateTimePicker1 As DateTimePicker
-    Friend WithEvents nameTextBox As TextBox
-    Friend WithEvents PhoneNumberTextBox As TextBox
-    Friend WithEvents Label6 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents icNumberTextBox As TextBox
-    Friend WithEvents matricTextBox As TextBox
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents IhsanTuitionCenterDbDataSetBindingSource As BindingSource
-    Friend WithEvents IhsanTuitionCenterDbDataSet As ihsanTuitionCenterDbDataSet
-    Friend WithEvents StudentBindingSource As BindingSource
-    Friend WithEvents StudentTableAdapter As ihsanTuitionCenterDbDataSetTableAdapters.studentTableAdapter
-    Friend WithEvents MatricNumberDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents StudentBindingSource1 As BindingSource
 End Class
